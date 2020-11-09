b979aed535e7766c6d83959e1de5308afca1c291
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-11-09 15:58:26 -0300

    Adiciona float com a posição do topo da lista

--- ARMDemo WatchKit Extension/ContentView.swift
+++ ARMDemo WatchKit Extension/ContentView.swift
@@ -8,41 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var exibirModal: Bool = false
-    @State private var textoFundo: String = "ARM"
+    @State private var posY: CGFloat = 0
+    @State private var backgroundColor: Color = .blue
+    private var elementos = ["CPFs", "Nomes", "Datas de Nascimento"]
+    
+    // O GeometryGetter vai de 28.0 a -164.0 (topo e pé do scroll view).
     
     var body: some View {
         VStack {
             ZStack {
-                Text(textoFundo)
+                Text("\(posY)")
                     .font(.title)
                     .bold()
                     .opacity(0.5)
                 
                 ScrollView {
-                    Text("🇬🇧")
-                        .font(.title)
-                    Text("🍎")
-                        .font(.title)
-                    Text("📱")
-                        .font(.title)
-                    Text("⌚️")
-                        .font(.title)
-                    Text("💻")
-                        .font(.title)
-                    Text("🌎")
-                        .font(.title)
-                    Text("João dos Santos")
-                        .font(.title3)
-                    Text("Leticia Sant'anna")
-                        .font(.title3)
-                    Text("Matheus Milanezi")
-                        .font(.title3)
-                    Text("Rafael Schmitt")
-                        .font(.title3)
-                }.background(Color.yellow)
+                    VStack {
+                        GeometryReader { innerGeo -> Text in
+                            self.posY = innerGeo.frame(in: .global).minY
+                            return Text("")
+                        }
+                        Text("🇬🇧")
+                            .font(.title)
+                        Text("🍎")
+                            .font(.title)
+                        Text("📱")
+                            .font(.title)
+                        Text("⌚️")
+                            .font(.title)
+                        Text("💻")
+                            .font(.title)
+                        Text("🌎")
+                            .font(.title)
+                        Text("João dos Santos")
+                            .font(.title3)
+                        Text("Leticia Sant'anna")
+                            .font(.title3)
+                        Text("Matheus Milanezi")
+                            .font(.title3)
+//                        Text("Rafael Schmitt")
+//                            .font(.title3)
+                    }
+                }
             }
-        }.background(Color.blue)
+        }.background(backgroundColor)
     }
 }
 
